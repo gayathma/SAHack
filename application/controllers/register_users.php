1d8db015fd6b6a30a395dc4440d85f826d0901bf
ashanidiaz <ashanidiaz@gmail.com>
2015-09-12 14:02:57 +0530

    rr

--- application/controllers/register_users.php
+++ application/controllers/register_users.php
@@ -23,14 +23,14 @@ class Register_Users extends CI_Controller {
         $register_users_model = new Register_Users_model();
         $register_users_service = new Register_Users_service();
 
-        $register_users_model->set_name($this->input->post('form_register_full_name', TRUE));
-        $register_users_model->set_user_name($this->input->post('form_register_user_name', TRUE));
-        $register_users_model->set_user_type('3');
-        $register_users_model->set_email(trim($this->input->post('form_register_email', TRUE)));
-        $register_users_model->set_address($this->input->post('form_register_address', TRUE));
-        $register_users_model->set_contact1($this->input->post('form_register_contact', TRUE));
-        $register_users_model->set_profile_pic('avatar.png');
-        $register_users_model->set_password(md5($this->input->post('form_register_password', TRUE)));        
+        $register_users_model->set_name($this->input->post('name', TRUE));
+        $register_users_model->set_user_name($this->input->post('user_name', TRUE));
+        $register_users_model->set_user_type('Admin','Manager','Surgeon','Op');
+        $register_users_model->set_email(trim($this->input->post('email', TRUE)));
+        $register_users_model->set_address($this->input->post('address', TRUE));
+        $register_users_model->set_contact1($this->input->post('contact_no_1', TRUE));
+        //$register_users_model->set_profile_pic('avatar.png');
+        $register_users_model->set_password(md5($this->input->post('password', TRUE)));        
         $register_users_model->set_is_deleted('0');
         
         $register_users_service->add_new_user_registration($register_users_model);
